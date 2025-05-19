780f5adb1aaa3a14055a60208397a4610f4f9794
yeeun-uwu <yesilver@ewha.ac.kr>
2025-05-19 14:33:16 +0900

    test : flask-react 연결 테스트 코드 추가가
    proxy 추가 및 테스트 코드 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,27 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
+  //flask 연결 테스트 코드 
+  const [data, setData] = useState({ members: [] });
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then(res => {
+        if (!res.ok) throw new Error("API 요청 실패");
+        return res.json();
+      })
+      .then(data => setData(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <h1>test 중</h1>
+      <div>
+        {data.members.map((u) => <p key={u.id}>{u.name}</p>)}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,12 @@
-from flask import Flask # Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/users')
 def users():
 	# users 데이터를 Json 형식으로 반환한다
-    return {"members": [{ "id" : 1, "name" : "test" },
-    					{ "id" : 2, "name" : "test2" }]}
+    return jsonify({"members": [{ "id" : 1, "name" : "test" },
+    					{ "id" : 2, "name" : "test2" }]})
            
 
 if __name__ == "__main__":
