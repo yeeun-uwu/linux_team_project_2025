66b636f9ad850fbb5bb28ae6ace23814d9115a48
yeeun-uwu <yesilver@ewha.ac.kr>
2025-05-26 16:06:09 +0900

    fix : api 수정정
    요청하는 데이터가 잘못되어 수정

--- flask-server/app/services/fetch_air_quality.py
+++ flask-server/app/services/fetch_air_quality.py
@@ -21,36 +21,36 @@ def fetch_air_quality():
     time = datetime.now()
     today = datetime.strftime(time, '%Y%m%d')
     #오늘 날짜 
-    logger.info(f"{time} 시각 에어코리아 시도별 실시간 측정정보를 요청합니다.")
+    logger.info(f"{time} 시각 에어코리아 측정소별 실시간 측정정보를 요청합니다.")
 
     # 기본 URL
-    base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
-    # 에어코리아 시도별 실시간 측정정보 조회 
+    base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
+    # 에어코리아 측정소별 실시간 측정정보 조회 
 
     # 쿼리 파라미터 구성
     params = {
         'serviceKey' : api_key["dec"],
         'returnType' : 'json',
-        'numOfRows': 100,
-        'pageNo' : 1,
-        'sidoName' : '서울',
-        'ver' : '1.3'
+        'numOfRows': '100',
+        'pageNo' : '1',
+        'stationName' : '서대문구',
+        'dataTerm' : 'DAILY',
+        'ver' : '1.1'
     }
 
     # 요청 보내기
     response = requests.get(base_url, params=params)
     data = response.json()
-    items  = data["response"]["body"]["items"]
-    ans = [item for item in items if item.get("stationName") == "서대문구"]
+    items  = data["response"]["body"]
 
     # 응답 상태 확인
     if response.status_code == 200:
         #print(ans)
         with open(BASE_DIR+"/data/air_quality.json", "w", encoding="utf-8") as f:
-            json.dump(ans, f, ensure_ascii= False, indent=2)
+            json.dump(items, f, ensure_ascii= False, indent=2)
         
-        logger.info(f"{today} 에어코리아 실시간 측정정보 저장 완료")
-        return ans
+        logger.info(f"{today} 에어코리아 측정소별 실시간 측정정보 저장 완료")
+        return items
     else:
         logger.error(f"오류 발생: {response.status_code}")
         return None
