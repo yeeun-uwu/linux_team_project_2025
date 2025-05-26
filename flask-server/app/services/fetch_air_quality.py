1c8e7efa2504cca97e335840bdbf328072ba0774
yeeun-uwu <yesilver@ewha.ac.kr>
2025-05-26 11:39:57 +0900

    feat : 에어코리아 api response 저장
    데이터 추출해서 저장하는 코드 추가

--- flask-server/app/services/fetch_air_quality.py
+++ flask-server/app/services/fetch_air_quality.py
@@ -18,6 +18,7 @@ today = datetime.strftime(time, '%Y%m%d')
 
 # 기본 URL
 base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
+# 에어코리아 시도별 실시간 측정정보 조회 
 
 # 쿼리 파라미터 구성
 params = {
@@ -31,11 +32,16 @@ params = {
 
 # 요청 보내기
 response = requests.get(base_url, params=params)
+data = response.json()
+items  = data["response"]["body"]["items"]
+ans = [item for item in items if item.get("stationName") == "서대문구"]
 
 # 응답 상태 확인
 if response.status_code == 200:
-    print(response.text)  # json 문자열 출력
+    #print(ans)
+    with open("flask-server/data/air_quality.json", "w", encoding="utf-8") as f:
+        json.dump(ans, f, ensure_ascii= False, indent=2)
+    
+    print(f"{today} 에어코리아 저장 완료")
 else:
     print(f"오류 발생: {response.status_code}")
-
-# api 호출 / 출력까지만 만들어둠 
