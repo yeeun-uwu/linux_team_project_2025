7035795b8a853261db92800b8b7a4d296d524e66
yeeun-uwu <yesilver@ewha.ac.kr>
2025-05-26 12:06:08 +0900

    feat : 로깅 추가
    서버 역할을 할 때 print보다 log가 유용해 추가

---
+++ flask-server/run.py
@@ -0,0 +1,11 @@
+from app import create_app
+from app.storage.logging_config import setup_logging
+
+# 로깅 설정
+setup_logging()
+
+#Flask 앱 생성/실행
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
