4e16b44d8e7d3e42e8e851311a70158d8e12611a
yeeun-uwu <yesilver@ewha.ac.kr>
2025-05-26 13:15:36 +0900

    feat : api 요청 / 데이터 저장 추가
    - api 요청하는 함수 추가
    - 요청된 api는 필요한 정보만 json 파일로 저장됨
    - run.py에 테스트 코드 추가

--- flask-server/app/services/fetch_air_quality.py
+++ flask-server/app/services/fetch_air_quality.py
@@ -1,47 +1,56 @@
-#에어코리아 api 파싱
+#에어코리아 api 요청
 
 from datetime import datetime, timedelta
 import json
 import pandas as pd
 import requests
+import os
 
+import logging
+logger = logging.getLogger(__name__)
 
-key_file = 'flask-server/api_key.json'
-with open(key_file) as f:
-    api_key = json.load(f)
-api_key = api_key["key"]
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
-time = datetime.now()
-print(time)
-today = datetime.strftime(time, '%Y%m%d')
-#오늘 날짜 
+def fetch_air_quality():
+      # app/services -> app -> flask-server
+    key_file = os.path.join(BASE_DIR, 'api_key.json')
+    with open(key_file) as f:
+        api_key = json.load(f)
+    api_key = api_key["key"]
 
-# 기본 URL
-base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
-# 에어코리아 시도별 실시간 측정정보 조회 
+    time = datetime.now()
+    today = datetime.strftime(time, '%Y%m%d')
+    #오늘 날짜 
+    logger.info(f"{time} 시각 에어코리아 시도별 실시간 측정정보를 요청합니다.")
 
-# 쿼리 파라미터 구성
-params = {
-    'serviceKey' : api_key["dec"],
-    'returnType' : 'json',
-    'numOfRows': 100,
-    'pageNo' : 1,
-    'sidoName' : '서울',
-    'ver' : '1.3'
-}
+    # 기본 URL
+    base_url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
+    # 에어코리아 시도별 실시간 측정정보 조회 
 
-# 요청 보내기
-response = requests.get(base_url, params=params)
-data = response.json()
-items  = data["response"]["body"]["items"]
-ans = [item for item in items if item.get("stationName") == "서대문구"]
+    # 쿼리 파라미터 구성
+    params = {
+        'serviceKey' : api_key["dec"],
+        'returnType' : 'json',
+        'numOfRows': 100,
+        'pageNo' : 1,
+        'sidoName' : '서울',
+        'ver' : '1.3'
+    }
 
-# 응답 상태 확인
-if response.status_code == 200:
-    #print(ans)
-    with open("flask-server/data/air_quality.json", "w", encoding="utf-8") as f:
-        json.dump(ans, f, ensure_ascii= False, indent=2)
-    
-    print(f"{today} 에어코리아 저장 완료")
-else:
-    print(f"오류 발생: {response.status_code}")
+    # 요청 보내기
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    items  = data["response"]["body"]["items"]
+    ans = [item for item in items if item.get("stationName") == "서대문구"]
+
+    # 응답 상태 확인
+    if response.status_code == 200:
+        #print(ans)
+        with open(BASE_DIR+"/data/air_quality.json", "w", encoding="utf-8") as f:
+            json.dump(ans, f, ensure_ascii= False, indent=2)
+        
+        logger.info(f"{today} 에어코리아 실시간 측정정보 저장 완료")
+        return ans
+    else:
+        logger.error(f"오류 발생: {response.status_code}")
+        return None
--- flask-server/run.py
+++ flask-server/run.py
@@ -1,5 +1,9 @@
 from app import create_app
 from app.storage.logging_config import setup_logging
+from app.services.fetch_air_quality import fetch_air_quality
+from app.services.fetch_forecast import fetch_forecast
+from app.services.fetch_realtime import fetch_realtime
+from app.services.fetch_indices import fetch_indices
 
 # 로깅 설정
 setup_logging()
@@ -8,4 +12,11 @@ setup_logging()
 app = create_app()
 
 if __name__ == "__main__":
+    # 함수 테스트하면 여기에 추가
+    fetch_air_quality()
+    fetch_forecast()
+    fetch_realtime()
+    fetch_indices()
+
+    # 서버 실행
     app.run(host="127.0.0.1", port=5000, debug=True)
